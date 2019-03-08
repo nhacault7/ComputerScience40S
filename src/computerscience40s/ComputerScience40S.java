91d4645018391f1ce2cea4aab933cce26f462270
nicholashacaulttabs@gmail.com <>
2019-03-08 12:12:28 -0600

    Current commit

---
+++ src/computerscience40s/ComputerScience40S.java
@@ -0,0 +1,21 @@
+package computerscience40s;
+
+import testing.RecursionTest;
+
+/**
+ * ComputerScience40S.java -
+ *
+ * @author Nicholas Hacault
+ * @since 20-Feb-2019
+ */
+public class ComputerScience40S {
+
+    public static void main(String[] args) {
+        System.out.println("CS40S starting...");
+        
+        RecursionTest unit1 = new RecursionTest();
+        
+        System.out.println("CS40S complete!");
+    }
+
+}
